9e0000ed2a59b29783673189d380a23899697322
Eric Sexton <foopis23@gmail.com>
2021-01-28 16:04:01 -0500

    More Ai Stuff. Still needs more work tho

--- Assets/Scripts/ZombieAI.cs
+++ Assets/Scripts/ZombieAI.cs
@@ -12,12 +12,23 @@ public class ZombieAI : MonoBehaviour
 
     public GameObject meshWrapper;
 
+    public float viewDistance;
+    public float agroViewDistance;
+    public float viewConeAngle;
+    public float agroViewConeAngle;
     public float attackDistance;
     public int attackTimeMs;
     public int attackCoolDownMs;
+    public int lookForLostPlayerMs;
+    public int timeToSpotMS;
+
+    private Vector3 lastSeenPlayerPos;
 
     //state flags
     private bool isAgro;
+    private bool hasLastPlayerPos;
+    private bool didSeePlayer;
+    private bool canSeePlayer;
     private bool isAttacking;
     private bool isAttackCoolingDown;
 
@@ -33,41 +44,90 @@ public class ZombieAI : MonoBehaviour
         if (animator == null)
             animator = GetComponent<Animator>();
 
-        isAgro = true;
+        lastSeenPlayerPos = new Vector3();
+        isAgro = false;
+        hasLastPlayerPos = false;
+        canSeePlayer = false;
+        didSeePlayer = false;
         isAttacking = false;
+        isAttackCoolingDown = false;
     }
 
     void Update()
     {
+        //reset some stuff
+        navMeshAgent.updatePosition = true;
+
+        //set animation set
         animator.SetBool("isAttacking", isAttacking);
         animator.SetBool("isAgro", isAgro);
         animator.SetBool("isAttackCoolingDown", isAttackCoolingDown);
 
+        didSeePlayer = canSeePlayer;
+        canSeePlayer = doesAISeePlayer();
+
         if (isAgro)
         {
-            if (!isAttacking)
+            if (canSeePlayer)
             {
-                navMeshAgent.isStopped = false;
-                navMeshAgent.SetDestination(target.position);
+                //chase and attack player
+                lastSeenPlayerPos = target.position;
+                hasLastPlayerPos = true;
 
-                //check if player is close enough to attack
-                if (navMeshAgent.remainingDistance <= attackDistance && !isAttackCoolingDown)
+                if (!isAttacking)
                 {
-                    isAttacking = true;
-                    EventSystem.Current.CallbackAfter(OnAttackFinished, attackTimeMs);
-                }
-            }
-            else
-            {
-                navMeshAgent.isStopped = true;
-            }
+                    navMeshAgent.SetDestination(target.position);
 
-            //check if player is still close enough for agro
+                    //check if player is close enough to attack
+                    if (navMeshAgent.remainingDistance <= attackDistance && !isAttackCoolingDown)
+                    {
+                        isAttacking = true;
+                        EventSystem.Current.CallbackAfter(OnAttackFinished, attackTimeMs);
+                    }
+                }
+                else
+                {
+                    navMeshAgent.updatePosition = false;
+                }
+            }else
+            {
+                //trigger a countdown for when to stop looking for the player
+                if (didSeePlayer)
+                {
+                    EventSystem.Current.CallbackAfter(OnLookingForPlayer, lookForLostPlayerMs);
+                }
+
+                if (hasLastPlayerPos)
+                {
+                    navMeshAgent.SetDestination(lastSeenPlayerPos);
+                }
+                else
+                {
+                    navMeshAgent.SetDestination(transform.position);
+                }
+
+            }
         }
         else
         {
-            //wander
+            //wander around
+            hasLastPlayerPos = false;
+            isAgro = canSeePlayer;
         }
+    }
+
+    bool doesAISeePlayer()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        RaycastHit hit;
+
+        float angle = (isAgro) ? agroViewConeAngle : viewConeAngle;
+        float distance = (isAgro) ? agroViewDistance : viewDistance;
+
+        return Mathf.Abs(Vector3.Angle(transform.position, target.position)) <= (angle / 2) &&
+            Vector3.Distance(transform.position, target.position) <= distance &&
+            Physics.Raycast(transform.position, direction, out hit) &&
+            hit.collider.gameObject.tag == "Player";
     }
 
     void OnAttackFinished()
@@ -80,5 +140,10 @@ public class ZombieAI : MonoBehaviour
     void OnAttackCooldownFinished()
     {
         isAttackCoolingDown = false;
+    }
+
+    void OnLookingForPlayer()
+    {
+        isAgro = canSeePlayer;
     }
 }
