754796a7cee0fd7b1ae775ef782dfe7c0ab75f08
James Dumas <jamesdumas418@gmail.com>
2021-01-29 00:08:20 -0500

    very epic map generation with terminals

--- Assets/Scripts/Terminal.cs
+++ Assets/Scripts/Terminal.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CallbackEvents;
 
 public class Terminal : MonoBehaviour
 {
+    // Automatic Properties
+    public Vector3 LinkedLevelLocation { get; set; }
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void HandleInteract()
+    {
+        //TODO: cool terminal camera zoom text interface puzzle hacking minigame desu
+
+        EventSystem.Current.FireEvent(new GenerateNextLevelContext(LinkedLevelLocation));
     }
 }
