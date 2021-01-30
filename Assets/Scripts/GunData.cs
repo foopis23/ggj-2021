637cef3faf720811699ba0ba558db5c25567ffa6
James Dumas <jamesdumas418@gmail.com>
2021-01-30 00:42:26 -0500

    guns exist even more

---
+++ Assets/Scripts/GunData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New GunData", menuName = "ScriptableObjects/GunData", order = 1)]
+public class GunData : ScriptableObject
+{
+    public string Name;
+    public float BulletDamage;
+    public float BulletRange;
+    public float FireCooldown;
+    public float Spread;
+    public int BulletsPerShot;
+    public bool Automatic;
+    public GameObject HitParticle;
+}
