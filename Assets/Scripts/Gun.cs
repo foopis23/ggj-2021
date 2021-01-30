7462f6210c609da5b5abf27589ba4c37fa05476c
James Dumas <jamesdumas418@gmail.com>
2021-01-29 20:16:31 -0500

    guns exist

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CallbackEvents;
+
+public class Gun : MonoBehaviour
+{
+    // public variables
+    public string Name;
+    public float BulletDamage;
+    public float BulletRange;
+    public float FireCooldown;
+    public float Spread;
+    public int BulletsPerShot;
+    public bool Automatic;
+    public GameObject HitParticle;
+
+    // private variables;
+    private float lastFire;
+    private Camera playerCamera;
+
+    void Start()
+    {
+        playerCamera = Camera.main;
+    }
+
+    public void Fire()
+    {
+        if(Time.time > lastFire + FireCooldown)
+        {
+            lastFire = Time.time;
+            for(int i = 0; i < BulletsPerShot; i++)
+            {
+                RaycastHit hit;
+                bool success = Physics.Raycast(playerCamera.transform.position, Quaternion.Euler(Random.value * Spread - (Spread * 0.5f), Random.value * Spread - (Spread * 0.5f), Random.value * Spread - (Spread * 0.5f)) * playerCamera.transform.forward, out hit, BulletRange);
+                if(success)
+                {
+                    EventSystem.Current.FireEvent(new BulletHitCtx(hit, 10));
+                    GameObject particleObject = Instantiate(HitParticle, hit.point, Quaternion.LookRotation(hit.normal));
+                    ParticleSystem particleSystem = particleObject.GetComponent<ParticleSystem>();
+                    Destroy(particleObject, particleSystem.main.duration + particleSystem.main.startLifetimeMultiplier);
+                }
+            }
+        }
+    }
+}
