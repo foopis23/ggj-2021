002f1124dee3ef6c0b943d69b0f07fbaaf33eba8
GuitarGaming <32265645+GuitarGaming@users.noreply.github.com>
2021-01-31 11:12:28 -0500

    WInMessage

---
+++ Assets/Scripts/WinMessage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class WinMessage : TerminalController
+{
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+
+        if (consoleElements.Count < 1)
+        {
+            if (Input.GetKeyDown("return") || Input.GetKeyDown("space"))
+            {
+                SceneManager.LoadScene(0);
+            }
+
+        }
+    }
+}
